17fd85ab24f156e6e43ea3c1d388fd535b65129a
erickpeirson <brp53@cornell.edu>
2018-02-02 13:30:22 -0500

    ARXIVNG-109 working on simple search template

--- search/controllers/simple/__init__.py
+++ search/controllers/simple/__init__.py
@@ -5,6 +5,9 @@ from search.services import index, fulltext, metadata
 from search.process import query
 from arxiv import status
 from search import logging
+from search.domain import SimpleQuery
+
+from .forms import SimpleSearchForm
 
 logger = logging.getLogger(__name__)
 
@@ -33,15 +36,20 @@ def search(request_params: dict) -> Response:
     dict
         Headers to add to the response.
     """
-    logger.debug('search request')
+    logger.debug('simple search form')
     response_data = {}
-    if 'q' in request_params:
-        q = query.prepare(request_params)
-        response_data['query'] = request_params['q']
+    form = SimpleSearchForm(request_params)
+    if form.validate():
+        logger.debug('form is valid')
+        q = _query_from_form(form)
         q = query.paginate(q, request_params)
         response_data.update(index.search(q))
+        response_data['query'] = q
     else:
+        logger.debug('form is invalid: %s' % str(form.errors))
         q = None
+        response_data['query'] = q
+    response_data['form'] = form
     return response_data, status.HTTP_200_OK, {}
 
 
@@ -72,3 +80,22 @@ def retrieve_document(document_id: str) -> Response:
     if result is None:
         return {'reason': 'No such paper'}, status.HTTP_404_NOT_FOUND, {}
     return {'document': result}, status.HTTP_200_OK, {}
+
+
+def _query_from_form(form: SimpleSearchForm) -> SimpleQuery:
+    """
+    Generate a :class:`.SimpleQuery` from valid :class:`.SimpleSearchForm`.
+
+    Parameters
+    ----------
+    form : :class:`.SimpleSearchForm`
+        Presumed to be filled and valid.
+
+    Returns
+    -------
+    :class:`.SimpleQuery`
+    """
+    query = SimpleQuery()
+    query.field = form.field.data
+    query.value = form.field.value
+    return query
