f52324620fc7a6c3f36d7c6c7586c99e5d55b1e1
Martin Lessmeister <mhl10@cornell.edu>
2018-03-29 09:06:24 -0400

    Fix style

--- search/controllers/advanced/__init__.py
+++ search/controllers/advanced/__init__.py
@@ -132,9 +132,8 @@ def _query_from_form(form: forms.AdvancedSearchForm) -> AdvancedQuery:
     q = _update_query_with_dates(q, form.date.data)
     q = _update_query_with_terms(q, form.terms.data)
     q = _update_query_with_classification(q, form.classification.data)
-    include_older_versions = form.include_older_versions.data
-    if include_older_versions:
-        q.include_older_versions = include_older_versions 
+    if form.include_older_versions.data:
+        q.include_older_versions = True
     order = form.order.data
     if order and order != 'None':
         q.order = order
