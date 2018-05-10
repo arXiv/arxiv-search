6371f59225318136b621b4985ecb07aba405ab87
erickpeirson <brp53@cornell.edu>
2018-05-10 12:55:17 -0400

    minor

--- search/services/index/util.py
+++ search/services/index/util.py
@@ -22,7 +22,7 @@ MAX_RESULTS = 10_000
 
 SPECIAL_CHARACTERS = ['+', '=', '&&', '||', '>', '<', '!', '(', ')', '{',
                       '}', '[', ']', '^', '~', ':', '\\', '/', '-']
-DEFAULT_SORT = ['_score', '-announced_date_first', '_doc']
+DEFAULT_SORT = ['-announced_date_first', '_doc']
 
 
 def wildcardEscape(querystring: str) -> Tuple[str, bool]:
