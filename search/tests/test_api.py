beb35ef3bb8a6b8615300d3a424f6eefe0b733e9
erickpeirson <brp53@cornell.edu>
2018-12-20 08:08:20 -0500

    added examples to openapi schema

--- search/tests/test_api.py
+++ search/tests/test_api.py
@@ -1,6 +1,8 @@
 from unittest import TestCase, mock
 
 from arxiv import taxonomy, status
+from arxiv.users.auth import scopes
+from arxiv.users.helpers import generate_token
 from search.factory import create_api_web_app
 
 
@@ -11,3 +13,6 @@ class TestAdvancedSearch(TestCase):
         """Instantiate the API application."""
         self.app = create_api_web_app()
         self.client = self.app.test_client()
+
+        self.token = generate_token('1', 'u@ser.com', 'theuser',
+                                    scope=[scopes.READ_PUBLIC])
