bf05185b6958e1385e5da768a055fe20a70aabc5
erickpeirson <brp53@cornell.edu>
2019-04-22 12:49:49 -0400

    added explanation for skipping SERVER_NAME when setting config from the WSGI environ

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,11 @@ __flask_app__ = create_ui_web_app()
 def application(environ, start_response):
     """WSGI application factory."""
     for key, value in environ.items():
+        # In some deployment scenarios (e.g. uWSGI on k8s), uWSGI will pass in
+        # the hostname as part of the request environ. This will usually just
+        # be a container ID, which is not helpful for things like building
+        # URLs. We want to keep ``SERVER_NAME`` explicitly configured, either
+        # in config.py or via an os.environ var loaded by config.py.
         if key == 'SERVER_NAME':
             continue
         os.environ[key] = str(value)
