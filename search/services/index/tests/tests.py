c22632ebb7ba8a18f8c7f506729873b88e178d8c
Jaimie Murdock <jmm698@cornell.edu>
2019-08-06 14:50:08 -0600

    adding test for id_list

--- search/services/index/tests/tests.py
+++ search/services/index/tests/tests.py
@@ -166,6 +166,45 @@ class TestSearch(TestCase):
         self.assertEqual(len(document_set['results']), 1)
 
 
+    @mock.patch('search.services.index.Search')
+    @mock.patch('search.services.index.Elasticsearch')
+    def test_classic_query_id_list(self, mock_Elasticsearch, mock_Search):
+        """:class:`.index.search` supports :class:`SimpleQuery`."""
+        mock_results = mock.MagicMock()
+        mock_results.__getitem__.return_value = {'total': 53}
+        rdata = dict(authors=[{'full_name': 'N. Ame'}],
+                     owners=[{'full_name': 'N. Ame'}],
+                     submitter={'full_name': 'N. Ame'},
+                     paper_id='1234.56789')
+        mock_result = mock.MagicMock(_d_=rdata, **rdata)
+        mock_result.meta.score = 1
+        mock_results.__iter__.return_value = [mock_result]
+        mock_Search.execute.return_value = mock_results
+
+        # Support the chaining API for py-ES.
+        mock_Search.return_value = mock_Search
+        mock_Search.filter.return_value = mock_Search
+        mock_Search.highlight.return_value = mock_Search
+        mock_Search.highlight_options.return_value = mock_Search
+        mock_Search.query.return_value = mock_Search
+        mock_Search.sort.return_value = mock_Search
+        mock_Search.__getitem__.return_value = mock_Search
+
+        query = ClassicAPIQuery(
+            id_list=['1234.56789'],
+            order='relevance',
+            size=10
+        )
+
+        document_set = index.SearchSession.search(query)
+        # self.assertIsInstance(document_set, DocumentSet)
+        self.assertEqual(document_set['metadata']['start'], 0)
+        self.assertEqual(document_set['metadata']['total_results'], 53)
+        self.assertEqual(document_set['metadata']['current_page'], 1)
+        self.assertEqual(document_set['metadata']['total_pages'], 6)
+        self.assertEqual(document_set['metadata']['size'], 10)
+        self.assertEqual(len(document_set['results']), 1)
+
 
 class TestWildcardSearch(TestCase):
     """A wildcard [*?] character is present in a querystring."""
