842581dbaa106290346d3880c45c87a2ce4fa1a8
Jaimie Murdock <jmm698@cornell.edu>
2019-08-12 23:31:55 -0400

    adding single-paper tests

--- search/routes/api/tests/test_api.py
+++ search/routes/api/tests/test_api.py
@@ -197,3 +197,79 @@ class TestAPISearchRequests(TestCase):
             set(data['results'][0].keys()),
             set(query.include_fields)
         )
+
+
+    @mock.patch(f'{factory.__name__}.api.api')
+    def test_paper_retrieval(self, mock_controller):
+        """Test single-paper retrieval."""
+        document = dict(
+            submitted_date=datetime.now(),
+            submitted_date_first=datetime.now(),
+            announced_date_first=datetime.now(),
+            id='1234.5678',
+            abstract='very abstract',
+            authors=[
+                dict(full_name='F. Bar', orcid='1234-5678-9012-3456')
+            ],
+            submitter=dict(full_name='S. Ubmitter', author_id='su_1'),
+            modified_date=datetime.now(),
+            updated_date=datetime.now(),
+            is_current=True,
+            is_withdrawn=False,
+            license={
+                'uri': 'http://foo.license/1',
+                'label': 'Notalicense 5.4'
+            },
+            paper_id='1234.5678',
+            paper_id_v='1234.5678v6',
+            title='tiiiitle',
+            source={
+                'flags': 'A',
+                'format': 'pdftotex',
+                'size_bytes': 2
+            },
+            version=6,
+            latest='1234.5678v6',
+            latest_version=6,
+            report_num='somenum1',
+            msc_class=['c1'],
+            acm_class=['z2'],
+            journal_ref='somejournal (1991): 2-34',
+            doi='10.123456/7890',
+            comments='very science',
+            abs_categories='astro-ph.CO foo.BR',
+            formats=['pdf', 'other'],
+            primary_classification=dict(
+                group={'id': 'foo', 'name': 'Foo Group'},
+                archive={'id': 'foo', 'name': 'Foo Archive'},
+                category={'id': 'foo.BR', 'name': 'Foo Category'},
+            ),
+            secondary_classification=[
+                dict(
+                    group={'id': 'foo', 'name': 'Foo Group'},
+                    archive={'id': 'foo', 'name': 'Foo Archive'},
+                    category={'id': 'foo.BZ', 'name': 'Baz Category'},
+                )
+            ]
+        )
+        docs = dict(
+            results=[document],
+            metadata={'start': 0, 'end': 1, 'size': 50, 'total': 1}
+        )
+        r_data = {'results': docs, 'query': domain.APIQuery()}
+        mock_controller.paper.return_value = r_data, status.HTTP_200_OK, {}
+        token = helpers.generate_token('1234', 'foo@bar.com', 'foouser',
+                                       scope=[auth.scopes.READ_PUBLIC])
+        response = self.client.get('/1234.56789v6', headers={'Authorization': token})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = json.loads(response.data)
+        res = jsonschema.RefResolver(
+            'file://%s/' % os.path.abspath(os.path.dirname(self.SCHEMA_PATH)),
+            None
+        )
+        self.assertIsNone(jsonschema.validate(data, self.schema, resolver=res),
+                          'Response content is valid per schema')
+
+        for field in domain.api.get_required_fields():
+            self.assertIn(field, data['results'][0])
--- search/routes/api/tests/test_classic.py
+++ search/routes/api/tests/test_classic.py
@@ -106,3 +106,68 @@ class TestClassicAPISearchRequests(TestCase):
             '/classic/query?search_query=au:copernicus',
             headers={'Authorization': token})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    @mock.patch(f'{factory.__name__}.api.classic.api')
+    def test_paper_retrieval(self, mock_controller):
+        """Test single-paper retrieval."""
+        document = dict(
+            submitted_date=datetime.now(pytz.utc),
+            submitted_date_first=datetime.now(pytz.utc),
+            announced_date_first=datetime.now(pytz.utc),
+            id='1234.5678',
+            abstract='very abstract',
+            authors=[
+                dict(full_name='F. Bar', orcid='1234-5678-9012-3456')
+            ],
+            submitter=dict(full_name='S. Ubmitter', author_id='su_1'),
+            modified_date=datetime.now(pytz.utc),
+            updated_date=datetime.now(pytz.utc),
+            is_current=True,
+            is_withdrawn=False,
+            license={
+                'uri': 'http://foo.license/1',
+                'label': 'Notalicense 5.4'
+            },
+            paper_id='1234.5678',
+            paper_id_v='1234.5678v6',
+            title='tiiiitle',
+            source={
+                'flags': 'A',
+                'format': 'pdftotex',
+                'size_bytes': 2
+            },
+            version=6,
+            latest='1234.5678v6',
+            latest_version=6,
+            report_num='somenum1',
+            msc_class=['c1'],
+            acm_class=['z2'],
+            journal_ref='somejournal (1991): 2-34',
+            doi='10.123456/7890',
+            comments='very science',
+            abs_categories='astro-ph.CO foo.BR',
+            formats=['pdf', 'other'],
+            primary_classification=dict(
+                group={'id': 'foo', 'name': 'Foo Group'},
+                archive={'id': 'foo', 'name': 'Foo Archive'},
+                category={'id': 'foo.BR', 'name': 'Foo Category'},
+            ),
+            secondary_classification=[
+                dict(
+                    group={'id': 'foo', 'name': 'Foo Group'},
+                    archive={'id': 'foo', 'name': 'Foo Archive'},
+                    category={'id': 'foo.BZ', 'name': 'Baz Category'},
+                )
+            ]
+        )
+        docs = dict(
+            results=[document],
+            metadata={'start': 0, 'end': 1, 'size': 50, 'total': 1}
+        )
+        r_data = {'results': docs, 'query': domain.APIQuery()}
+        mock_controller.paper.return_value = r_data, status.HTTP_200_OK, {}
+        token = helpers.generate_token('1234', 'foo@bar.com', 'foouser',
+                                       scope=[auth.scopes.READ_PUBLIC])
+        response = self.client.get('/classic/1234.56789v6', headers={'Authorization': token})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
