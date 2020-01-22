b0e7b3624b7857c094fe2b99d5983c8952b323cc
Martin Lessmeister <mhl10@cornell.edu>
2020-01-22 16:31:59 -0500

    pydocstyle fixes

--- search/errors.py
+++ search/errors.py
@@ -1,20 +1,31 @@
+"""Search error classes."""
+
+
 class SearchError(Exception):
+    """Generic search error."""
+
     def __init__(self, message: str):
+        """Initialize the error message."""
         self.message = message
 
     @property
     def name(self) -> str:
+        """Error name."""
         return self.__class__.__name__
 
     def __str__(self) -> str:
+        """Represent error as a string."""
         return f"{self.name}({self.message})"
 
     __repr__ = __str__
 
 
 class ValidationError(SearchError):
+    """Validation error."""
+
     def __init__(
         self, message: str, link: str = "http://arxiv.org/api/errors"
     ):
+        """Initialize the validation error."""
         super().__init__(message=message)
         self.link = link
