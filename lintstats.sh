a6809f90213a3809e15c81a66be4569883647462
Martin Lessmeister <mhl10@cornell.edu>
2020-02-20 22:03:17 -0500

    minor

--- lintstats.sh
+++ lintstats.sh
@@ -16,7 +16,7 @@ curl -u $USERNAME:$GITHUB_TOKEN \
 MYPY_STATUS=$( pipenv run mypy -p search | grep -v "test.*" | grep -v "defined here" | tee /dev/tty | wc -l | tr -d '[:space:]' )
 if [ $MYPY_STATUS -ne 0 ]; then MYPY_STATE="failure" && echo "mypy failed"; else MYPY_STATE="success" &&  echo "mypy passed"; fi
 
-curl -u $USERNAME:$GITHUB_TOKEN \
+curl -v -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$MYPY_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "code-quality/mypy"}' \
     -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
     > /dev/null 2>&1
