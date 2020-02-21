5edf6ab1a91fab569922daa76ccfd32129ef9873
Martin Lessmeister <mhl10@cornell.edu>
2020-02-20 22:23:09 -0500

    minor

--- lintstats.sh
+++ lintstats.sh
@@ -6,10 +6,10 @@ PYLINT_PASS=$(echo $PYLINT_SCORE">="$MIN_SCORE | bc -l)
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
 if [ "$PYLINT_PASS" ]; then PYLINT_STATE="success" &&  echo "pylint passed with score "$PYLINT_SCORE" for sha "$SHA; else PYLINT_STATE="failure" &&  echo "pylint failed with score "$PYLINT_SCORE" for sha "$SHA; fi
 
-curl -v -u $USERNAME:$GITHUB_TOKEN \
+curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$PYLINT_STATE'", "target_url": "https://travis-ci.com/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "'$PYLINT_SCORE'/10", "context": "code-quality/pylint"}' \
-    -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA 
-
+    -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
+    > /dev/null 2>&1
 
 
 # Check mypy integration
