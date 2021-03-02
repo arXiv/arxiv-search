08031834edc58e35883e6b301781a86268d02b75
Brian Maltzan <bgm37@cornell.edu>
2021-03-02 16:36:10 -0500

    monkeypatch es.search() to use post

--- search/services/index/__init__.py
+++ search/services/index/__init__.py
@@ -11,6 +11,24 @@ for indexing (e.g. by the
 :mod:`search.agent.consumer.MetadataRecordProcessor`).
 """
 
+
+# Start monkeypatch of elasticsearch-py's search(), use POST not GET
+
+import elasticsearch.client
+
+def search2(self, index=None, doc_type=None, body=None, params=None):
+  if params and 'from_' in params:
+    params['from'] = params.pop('from_')
+  if doc_type and not index:
+    index = '_all'
+  tmppath =  elasticsearch.client.utils._make_path(index, doc_type, '_search')
+  return self.transport.perform_request('POST', tmppath, params=params, body=body)
+
+elasticsearch.client.Elasticsearch.search = search2
+
+# End monkeypatch
+
+
 __all__ = ["Q", "SearchSession"]
 
 import json
