03815c9c1c391dc3d6984512e53a5f748cf3aedd
Brian Maltzan <bgm37@cornell.edu>
2021-03-30 15:13:56 -0400

    test-travis

--- lintstats.sh
+++ lintstats.sh
@@ -4,9 +4,7 @@ PYLINT_SCORE=$( pipenv run pylint search | tail -2 | grep -Eo '[0-9\.]+/10' | ta
 PYLINT_PASS=$(echo $PYLINT_SCORE">="$MIN_SCORE | bc -l)
 
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
-if [ "$PYLINT_PASS" ]; then PYLINT_STATE="success" &&  echo "pylint passed with score "$PYLINT_SCORE" for sha "$SHA; else PYLINT_STATE="failure" &&  echo "pylint failed with score "$PYLINT_SCORE" for sha "$SHA; fi
-
-echo "Debugging: PYLINT_SCORE=$PYLINT_SCORE, MIN_SCORE=$MIN_SCORE, PYLINT_PASS=$PYLINT_PASS, PYLINT_STATE=${PYLINT_STATE}, SHA=$SHA, TRAVIS_COMMIT=$TRAVIS_COMMIT, TRAVIS_PULL_REQUEST_SHA=$TRAVIS_PULL_REQUEST_SHA"
+if [ $PYLINT_PASS -eq 1 ]; then PYLINT_STATE="success" &&  echo "pylint passed with score "$PYLINT_SCORE" for sha "$SHA; else PYLINT_STATE="failure" &&  echo "pylint failed with score "$PYLINT_SCORE" for sha "$SHA; fi
 
 curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$PYLINT_STATE'", "target_url": "https://travis-ci.com/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "'$PYLINT_SCORE'/10", "context": "code-quality/pylint"}' \
@@ -21,7 +19,6 @@ curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$MYPY_STATE'", "target_url": "https://travis-ci.org/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "", "context": "code-quality/mypy"}' \
     -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA \
     > /dev/null 2>&1
-
 
 
 # Check pydocstyle integration
