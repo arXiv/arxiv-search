f4adfecc22752b31f31a8bebd43c6c52e9ac818f
Brian Maltzan <bgm37@cornell.edu>
2021-03-30 15:01:40 -0400

    test-travis

--- lintstats.sh
+++ lintstats.sh
@@ -6,6 +6,8 @@ PYLINT_PASS=$(echo $PYLINT_SCORE">="$MIN_SCORE | bc -l)
 if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
 if [ "$PYLINT_PASS" ]; then PYLINT_STATE="success" &&  echo "pylint passed with score "$PYLINT_SCORE" for sha "$SHA; else PYLINT_STATE="failure" &&  echo "pylint failed with score "$PYLINT_SCORE" for sha "$SHA; fi
 
+echo "Debugging: PYLINT_SCORE=$PYLINT_SCORE, MIN_SCORE=$MIN_SCORE, PYLINT_PASS=$PYLINT_PASS, PYLINT_STATE=${PYLINT_STATE}, SHA=$SHA, TRAVIS_COMMIT=$TRAVIS_COMMIT, TRAVIS_PULL_REQUEST_SHA=$TRAVIS_PULL_REQUEST_SHA"
+
 curl -u $USERNAME:$GITHUB_TOKEN \
     -d '{"state": "'$PYLINT_STATE'", "target_url": "https://travis-ci.com/'$TRAVIS_REPO_SLUG'/builds/'$TRAVIS_BUILD_ID'", "description": "'$PYLINT_SCORE'/10", "context": "code-quality/pylint"}' \
     -XPOST https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$SHA 
